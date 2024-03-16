e796f0bc8d41d9fc54ee26e96c387a82cd434b17
ABDURRAHMAN34 <imranyigit32@gmail.com>
2024-03-16 17:03:10 +0300

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -42,7 +42,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::inRandomOrder('id')->paginate(10);
+        $posts = Post::inRandomOrder('id')->paginate(15);
         return PostResource::collection($posts);
     }
 
